3898b39f1bc24789b3346379a8b662494aeb2fbd
shyamcelsior <shyamagarwal.hbti@gmail.com>
2022-09-20 11:08:59 +0530

    commit by shyam under GitDemo

--- src/Jdbcconnection.java
+++ src/Jdbcconnection.java
@@ -14,7 +14,7 @@ public class Jdbcconnection {
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		
-		String host="127.0.0.1";
+		String host="localhost";
 		String port="3306";
 		
 		Connection con=DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/qadbtesting", "root", "root");
